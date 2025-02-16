dc8925ca7f08233bfdb8ad13bb6629781ffccaf8
eunho <76768140sa@gmail.com>
2025-02-17 00:46:14 +0900

    [feat] create todo 구현

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,28 +1,57 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.repository.TodoRepository;
 
+import jakarta.persistence.Entity;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class TodoService {
-	
+
 	@Autowired
 	private TodoRepository repository;
-	
+
 	public String testService() {
 		// TodoEntity 생성
-		TodoEntity entity = TodoEntity.builder()
-										.title("My first todo item")
-										.build();
+		TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
 		// TodoEntity 저장
 		repository.save(entity);
-		
+
 		// TodoEntity 검색
 		TodoEntity savedEntity = repository.findById(entity.getId()).get();
-		
+
 		return savedEntity.getTitle();
+	}
+
+	public List<TodoEntity> create(final TodoEntity entity) {
+		// Validations
+		validate(entity);
+
+		repository.save(entity);
+
+		log.info("Entity Id : {} is saved.", entity.getId());
+
+		return repository.findByUserId(entity.getUserId());
+	}
+
+	// 리팩토링한 메소드
+	private void validate(final TodoEntity entity) {
+		// Validations
+		if (entity == null) {
+			log.warn("Entity cannot be null.");
+			throw new RuntimeException("Entity cannot be null.");
+		}
+
+		if (entity.getUserId() == null) {
+			log.warn("Entity cannot be null.");
+			throw new RuntimeException("Entity cannot be null.");
+		}
 	}
 }
