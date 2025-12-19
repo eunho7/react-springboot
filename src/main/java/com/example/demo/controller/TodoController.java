09cb99d46a69c0c8a601988f19f5404c277c876b
eunho <76768140sa@gmail.com>
2025-12-19 18:03:24 +0900

    Feat: todo retrieve 서비스 구현

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,14 +1,16 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -25,6 +27,56 @@ public class TodoController {
         List<String> list = new ArrayList<>();
         list.add(str);
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    // create todo
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO todoDTO) {
+        try {
+            String temporaryUserId = "temporary-user"; // temporary user id.
+
+            // (1) TodoEntity로 변환한다.
+            TodoEntity entity = TodoDTO.toEntity(todoDTO);
+
+            // (2) id를 null로 초기화한다. 생성 당시에는 id가 없어야 하기 때문이다.
+            entity.setId(null);
+
+            // (3) 사용자 임시 id 부여한다.
+            entity.setUserId(temporaryUserId);
+
+            // (4) 서비스를 이용해 Todo 엔티티를 생성한다.
+            List<TodoEntity> entities = todoService.create(entity);
+
+            // (5) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            // (6) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화 한다.
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            // (7) ResponseDTO를 리턴한다.
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    // read todo
+    @GetMapping
+    public ResponseEntity<?> readTodoList() {
+        String userId = "temporary-user"; // temporary user id.
+
+        // (1) 서비스 메서드의 read() 메서드를 사용해 Todo 리스트를 찾는다.
+        List<TodoEntity> entities = todoService.read(userId);
+
+        // (2) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        // (3) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화 한다.
+        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
 
         return ResponseEntity.ok().body(response);
     }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -3,8 +3,12 @@ package com.example.demo.service;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class TodoService {
@@ -19,5 +23,35 @@ public class TodoService {
         // TodoEntity 검색
         TodoEntity savedEntity = todoRepository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    // create todo
+    public List<TodoEntity> create(final TodoEntity entity) {
+        // Validations
+        validate(entity);
+
+        todoRepository.save(entity);
+
+        log.info("Entity Id : {} is saved", entity.getId());
+
+        return todoRepository.findByUserId(entity.getUserId());
+    }
+
+    // Read todo
+    public List<TodoEntity> read(final String userId) {
+        return todoRepository.findByUserId(userId);
+    }
+
+    // 리팩토링한 메소드
+    private void validate(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+
+        if (entity.getUserId() == null) {
+            log.warn("Unknown user");
+            throw new RuntimeException("Unknown user");
+        }
     }
 }
